77cb13f0eace7c9e5cc74f971fcf2a15504a30b2
Vitaliy <theyamshikov@yandex.ru>
2019-10-27 16:42:12 +0600

    444

--- config/eloquent/connection.php
+++ config/eloquent/connection.php
@@ -1,21 +1,38 @@
 <?php
 
+use App\Rails\Eloquent\Db\Enum\DbDriverEnum;
+
 return [
-    'default' => [
-        "driver" => 'mysql',
-        "host" => 'localhost',
-        "database" => 'symfony4',
-        "username" => 'root',
-        "password" => '',
-        "charset" => "utf8",
-        "collation" => "utf8_unicode_ci",
-        "prefix" => "",
+    //'defaultConnection' => 'mysqlServer',
+    //'defaultConnection' => 'sqliteServer',
+    //'defaultConnection' => 'pgsqlServer',
+    'connections' => [
+        'mysqlServer' => [
+            "driver" => 'mysql',
+            "host" => 'localhost',
+            "database" => 'symfony4',
+            "username" => 'root',
+            "password" => '',
+            "charset" => "utf8",
+            "collation" => "utf8_unicode_ci",
+            "prefix" => "",
+        ],
+        'sqliteServer' => [
+            "driver" => 'sqlite',
+            "database" => __DIR__ . '/../../var/sqlite/default.sqlite',
+            "charset" => "utf8",
+            "collation" => "utf8_unicode_ci",
+            "prefix" => "",
+        ],
+        'pgsqlServer' => [
+            "driver" => DbDriverEnum::PGSQL,
+            "host" => 'localhost',
+            "database" => 'symfony4',
+            "username" => 'postgres',
+            "password" => 'postgres',
+            "charset" => "utf8",
+            "collation" => "utf8_unicode_ci",
+            "prefix" => "",
+        ],
     ],
-    /*'slave' => [
-        "driver" => 'sqlite',
-        "database" => __DIR__ . '/../../var/sqlite/default.sqlite',
-        "charset" => "utf8",
-        "collation" => "utf8_unicode_ci",
-        "prefix" => "",
-    ],*/
 ];
--- src/Rails/Eloquent/Db/Helper/ManagerFactory.php
+++ src/Rails/Eloquent/Db/Helper/ManagerFactory.php
@@ -4,12 +4,14 @@ namespace App\Rails\Eloquent\Db\Helper;
 
 use App\Rails\Eloquent\Db\Enum\DbDriverEnum;
 use Illuminate\Database\Capsule\Manager;
+use php7extension\yii\helpers\ArrayHelper;
 use php7extension\yii\helpers\FileHelper;
 
 class ManagerFactory
 {
 
-    public static function createManager(array $connections) : Manager {
+    public static function createManager(array $config) : Manager {
+        $connections = self::getConnections($config);
         $capsule = new Manager;
         $capsule->setAsGlobal();
         foreach ($connections as $connectionName => $config) {
@@ -25,14 +27,34 @@ class ManagerFactory
      * Если файл БД не создан, то создает пустой файл
      *
      * @param string $connectionName
+     * @return void
      */
-    public static function forgeDb(string $connectionName = 'default') {
+    public static function forgeDb(string $connectionName = 'default') : void {
         $schema = Manager::schema($connectionName);
         $driver = $schema->getConnection()->getConfig('driver');
         if($driver == DbDriverEnum::SQLITE) {
             $database = $schema->getConnection()->getConfig('database');
             FileHelper::touch($database);
         }
+    }
+
+    private static function getConnections(array $config) : array {
+        $defaultConnection = ArrayHelper::getValue($config, 'defaultConnection');
+        $connections = ArrayHelper::getValue($config, 'connections', []);
+
+        if(empty($defaultConnection)) {
+            if( ! empty($connections['default'])) {
+                $defaultConnection = 'default';
+            } else {
+                $defaultConnection = ArrayHelper::firstKey($connections);
+            }
+        }
+
+        if($defaultConnection != 'default') {
+            $connections['default'] = $connections[$defaultConnection];
+            unset($connections[$defaultConnection]);
+        }
+        return $connections;
     }
 
 }
