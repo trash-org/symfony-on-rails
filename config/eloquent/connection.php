551cef1e77ac1fc847348128c3ce1f868e70a893
Vitaliy <theyamshikov@yandex.ru>
2019-10-27 16:45:11 +0600

    5555

--- config/eloquent/connection.php
+++ config/eloquent/connection.php
@@ -3,7 +3,7 @@
 use App\Rails\Eloquent\Db\Enum\DbDriverEnum;
 
 return [
-    //'defaultConnection' => 'mysqlServer',
+    'defaultConnection' => 'mysqlServer',
     //'defaultConnection' => 'sqliteServer',
     //'defaultConnection' => 'pgsqlServer',
     'connections' => [
@@ -12,17 +12,10 @@ return [
             "host" => 'localhost',
             "database" => 'symfony4',
             "username" => 'root',
-            "password" => '',
-            "charset" => "utf8",
-            "collation" => "utf8_unicode_ci",
-            "prefix" => "",
         ],
         'sqliteServer' => [
             "driver" => 'sqlite',
             "database" => __DIR__ . '/../../var/sqlite/default.sqlite',
-            "charset" => "utf8",
-            "collation" => "utf8_unicode_ci",
-            "prefix" => "",
         ],
         'pgsqlServer' => [
             "driver" => DbDriverEnum::PGSQL,
@@ -30,9 +23,6 @@ return [
             "database" => 'symfony4',
             "username" => 'postgres',
             "password" => 'postgres',
-            "charset" => "utf8",
-            "collation" => "utf8_unicode_ci",
-            "prefix" => "",
         ],
     ],
 ];
--- src/Rails/Eloquent/Db/Helper/ManagerFactory.php
+++ src/Rails/Eloquent/Db/Helper/ManagerFactory.php
@@ -58,3 +58,40 @@ class ManagerFactory
     }
 
 }
+
+/* Пример конфига
+return [
+    'defaultConnection' => 'mysqlServer',
+    //'defaultConnection' => 'sqliteServer',
+    //'defaultConnection' => 'pgsqlServer',
+    'connections' => [
+        'mysqlServer' => [
+            "driver" => 'mysql',
+            "host" => 'localhost',
+            "database" => 'symfony4',
+            "username" => 'root',
+            "password" => '',
+            "charset" => "utf8",
+            "collation" => "utf8_unicode_ci",
+            "prefix" => "",
+        ],
+        'sqliteServer' => [
+            "driver" => 'sqlite',
+            "database" => __DIR__ . '/../../var/sqlite/default.sqlite',
+            "charset" => "utf8",
+            "collation" => "utf8_unicode_ci",
+            "prefix" => "",
+        ],
+        'pgsqlServer' => [
+            "driver" => DbDriverEnum::PGSQL,
+            "host" => 'localhost',
+            "database" => 'symfony4',
+            "username" => 'postgres',
+            "password" => 'postgres',
+            "charset" => "utf8",
+            "collation" => "utf8_unicode_ci",
+            "prefix" => "",
+        ],
+    ],
+];
+ */
