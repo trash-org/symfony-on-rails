951f7e479b31c6da73be245d12d9557b66fa5c6a
Vitaliy <theyamshikov@yandex.ru>
2019-10-27 19:57:13 +0600

    444

--- src/Bundle/User/Domain/Service/AuthService.php
+++ src/Bundle/User/Domain/Service/AuthService.php
@@ -11,7 +11,7 @@ class AuthService
     public function authentication(AuthForm $form) : Identity {
         $identity = new Identity;
         $identity->id = 1;
-        $identity->login = $form->login;
+        $identity->login = 'admin';
         $identity->token = '1234567890987654321';
         return $identity;
     }
