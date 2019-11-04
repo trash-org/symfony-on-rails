01f8c46196849ca98569cdf27a5571d5f1ef1028
Vitaliy <theyamshikov@yandex.ru>
2019-11-04 21:22:51 +0600

    7777

--- src/Fixture/fos_user.php
+++ src/Fixture/fos_user.php
@@ -1,18 +1,20 @@
 <?php
 
-return [
-	[
-		'id' => 1,
-		'username' => 'admin',
-		'username_canonical' => 'admin',
-		'email' => 'user@yandex.ru',
-		'email_canonical' => 'user@yandex.ru',
-		'enabled' => 1,
-		'salt' => null,
-		'password' => '$2y$13$2BjMn.uhY8Yal6kICMoN.OuOIinRKmX7ld/sCJhGd6rpUjAR9d3DG',
-		'last_login' => '2019-10-27 18:11:21',
-		'confirmation_token' => null,
-		'password_requested_at' => null,
-		'roles' => 'a:0:{}',
-	],
-];
+use App\Rails\Eloquent\Fixture\Helper\FixtureFactoryHelper;
+
+$callback = function($index) {
+    $username = 'user' . $index;
+    return [
+        'id' => $index,
+        'username' => $username,
+        'username_canonical' => $username,
+        'email' => $username . '@example.com',
+        'email_canonical' => $username . '@example.com',
+        'enabled' => 1,
+        'password' => '$2y$13$2BjMn.uhY8Yal6kICMoN.OuOIinRKmX7ld/sCJhGd6rpUjAR9d3DG',
+        'last_login' => '2019-10-27 18:11:21',
+        'roles' => 'a:0:{}',
+    ];
+};
+
+return FixtureFactoryHelper::createCollection($callback, 10);
