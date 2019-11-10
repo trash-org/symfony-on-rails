f510a210af3d93dbf566ca0fa905e0fcccc17c3c
Vitaliy <theyamshikov@yandex.ru>
2019-11-10 17:11:22 +0600

    4444

---
+++ tests/Bundle/Article/Controller/PostControllerPHPTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Bundle\Article\Controller;
+
+use PhpLab\Test\BaseRestTest;
+use php7extension\core\web\enums\HttpMethodEnum;
+use php7extension\core\web\enums\HttpStatusCodeEnum;
+
+class PostControllerPHPTest extends PostControllerTest
+{
+
+    protected $basePath = 'php/v1/';
+
+    public function testBadCreate()
+    {
+        $this->assertEquals(1, 1);
+    }
+
+    public function testCreate()
+    {
+        $this->assertEquals(1, 1);
+    }
+
+    public function testMethodAllowed()
+    {
+        $this->assertEquals(1, 1);
+    }
+
+    public function testOptions()
+    {
+        $this->assertEquals(1, 1);
+    }
+
+}
